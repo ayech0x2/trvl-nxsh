5b084c8b34a96fa5e298bb1b1dbe2a29421f95d9
Hamza Ayech <4yech.hamza@gmail.com>
2025-11-27 17:44:23 +0100

    feat: integrate search engine modal with date and guest selection

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+import Cover from "@/components/ui/cover";
+import SearchEngine from "@/components/ui/search-engine";
+import SearchEngineModal from "@/components/ui/search-engine-modal";
+import * as React from "react";
+import { useIsMobile } from "./_hooks/use-is-mobile";
+import useSearchEngine from "./_hooks/use-search-engine";
+
 export default function Home() {
-  return <div></div>;
+  const isMobile = useIsMobile();
+
+  const { isOpen, setIsOpen, type, handleOpenChange, setSearchEngineState } =
+    useSearchEngine();
+
+  return (
+    <React.Fragment>
+      {isMobile ? (
+        <React.Fragment>
+          <Cover.Mobile />
+          <SearchEngine.Mobile handleOpenChange={handleOpenChange} />
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Cover.Web />
+          <SearchEngine.Web />
+        </React.Fragment>
+      )}
+      <SearchEngineModal
+        title={type === "DATE" ? "Select dates" : "Guests & rooms"}
+        type={type}
+        setSearchEngineState={setSearchEngineState}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+    </React.Fragment>
+  );
 }
